9b755ffbef68e7c2ce09e4acc02e6dc6eeeef96b
yishak <yishak.110402@gmail.com>
2024-08-04 01:11:28 +0300

    first commit

---
+++ src/components/AboutUs/CompanyValues/CompanyValues.js
@@ -0,0 +1,59 @@
+import "./CompanyValues.css";
+export default function CompanyValues() {
+  return (
+    <div className="our-values">
+      <h1>Values</h1>
+      <div className="values-container">
+        <div>
+          <h1>Dignity and Respect</h1>
+          <p>
+            Upholding the inherent dignity of every individual, advocating for
+            their rights, and ensuring they are treated with respect.
+          </p>
+        </div>
+        <div>
+          <h1>Equality and Justice</h1>
+          <p>
+            Committed to promoting equality and addressing all forms of
+            discrimination, striving for fairness and justice in every aspect of
+            our work.
+          </p>
+        </div>
+        <div>
+          <h1>Inclusion and Empowerment</h1>
+          <p>
+            Creating inclusive environments and empowering marginalized groups
+            to actively participate in and contribute to society.
+          </p>
+        </div>
+        <div>
+          <h1>Sustainability and Responsibility</h1>
+          <p>
+            Prioritizing environmental sustainability and advocating for
+            practices that protect and preserve our planet for future
+            generations.
+          </p>
+        </div>
+        <div>
+          <h1>Transparency and Integrity</h1>
+          <p>Upholding the highest ethical standards in all our activities.</p>
+        </div>
+        <div>
+          <h1 className="long">Environmental Ethics and</h1>
+          <h1> Eco-centrism</h1>
+          <p>
+            Emphasizing the importance of ecological balance and ethical
+            treatment of the environment.
+          </p>
+        </div>
+        <div>
+          <h1>Innovation</h1>
+          <p>
+            Encouraging creative solutions and forward-thinking approaches to
+            advance human rights and inclusion.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,13 @@
 import CompanyDescription from "../components/AboutUs/CompanyDescription/CompanyDescription";
-import CompanyEthics from "../components/AboutUs/CompanyValues/CompanyEthics";
+import CompanyEthics from "../components/AboutUs/CompanyEthics/CompanyEthics";
+import CompanyValues from "../components/AboutUs/CompanyValues/CompanyValues";
 
 export default function AboutUs() {
   return (
     <>
       <CompanyDescription />
       <CompanyEthics />
+      <CompanyValues/>
     </>
   );
 }
