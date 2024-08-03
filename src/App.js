1455fedd5056fdbf01d0d8abc23d9a2753026bad
yishak <yishak.110402@gmail.com>
2024-08-03 21:24:44 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Homepage from "./pages/Homepage";
+import Footer from "./components/Footer/Footer";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Navbar/>
-    <Routes>
-      <Route element={<Homepage/>} path="/" />
-    </Routes>
-
+      <Navbar />
+      <Routes>
+        <Route element={<Homepage />} path="/" />
+      </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
-
--- src/components/Homepage/Activities/Activities.js
+++ src/components/Homepage/Activities/Activities.js
@@ -1,35 +1,56 @@
-import "./Activities.css"
-export default function Activities(){
-    return(
-        <div className="activities">
-            <h1>Our Activities</h1>
-            <h5>Our activities encompass a broad range of initiatives aimed at fostering a more just and inclusive society:</h5>
-            <ul className="activities-container">
-                <li>
-                Advocacy and Awareness Campaigns: Raising awareness about human rights, gender equality, children’s rights, and environmental sustainability through community outreach and media campaigns.
-                </li>
-                <li>
-                Education and Training Programs: Conducting workshops, seminars, and training and educational programs to empower individuals and communities with knowledge about their rights and how to advocate for them, and to raise awareness and build capacity on human rights and inclusion.
-                </li>
-                <li>
-                Free Legal Aid Services: We provide comprehensive free legal aid to individuals who lack access to justice. This includes legal representation, counseling, and support to ensure fair treatment and justice for all.
-                </li>
-                <li>
-                Community Support: Providing resources, counseling, and support services to marginalized individuals and communities.
-                </li>
-                <li>
-                Community Dialogues and Engagement: Facilitating dialogues and engagement activities to promote civic participation and democratic governance.
-                </li>
-                <li>
-                Research and Publications: Conducting research on critical issues related to human rights, inclusion, and governance, and publishing findings to inform policy and practice.
-                </li>
-                <li>
-                Public Interest Litigation: Engaging in legal actions to protect the rights of vulnerable groups and to ensure environmental justice and protect environmental rights in Ethiopia.
-                </li>
-                <li>
-                Democracy Promotion: Encouraging active participation in democratic processes and monitoring elections to ensure fairness and transparency.
-                </li>
-            </ul>
-        </div>
-    )
+import "./Activities.css";
+export default function Activities() {
+  return (
+    <div className="activities">
+      <h1>Our Activities</h1>
+      <h5>
+        Our activities encompass a broad range of initiatives aimed at fostering
+        a more just and inclusive society:
+      </h5>
+      <ul className="activities-container">
+        <li>
+          <span>Advocacy and Awareness Campaigns</span>: Raising awareness about
+          human rights, gender equality, children’s rights, and environmental
+          sustainability through community outreach and media campaigns.
+        </li>
+        <li>
+          <span> Education and Training Programs</span>: Conducting workshops,
+          seminars, and training and educational programs to empower individuals
+          and communities with knowledge about their rights and how to advocate
+          for them, and to raise awareness and build capacity on human rights
+          and inclusion.
+        </li>
+        <li>
+          <span>Free Legal Aid Services</span>: We provide comprehensive free legal aid to
+          individuals who lack access to justice. This includes legal
+          representation, counseling, and support to ensure fair treatment and
+          justice for all.
+        </li>
+        <li>
+          <span>Community Support</span>: Providing resources, counseling, and support
+          services to marginalized individuals and communities.
+        </li>
+        <li>
+          <span>Community Dialogues and Engagement</span>: Facilitating dialogues and
+          engagement activities to promote civic participation and democratic
+          governance.
+        </li>
+        <li>
+          <span>Research and Publications</span>: Conducting research on critical issues
+          related to human rights, inclusion, and governance, and publishing
+          findings to inform policy and practice.
+        </li>
+        <li>
+          <span>Public Interest Litigation</span>: Engaging in legal actions to protect the
+          rights of vulnerable groups and to ensure environmental justice and
+          protect environmental rights in Ethiopia.
+        </li>
+        <li>
+          <span>Democracy Promotion</span>: Encouraging active participation in democratic
+          processes and monitoring elections to ensure fairness and
+          transparency.
+        </li>
+      </ul>
+    </div>
+  );
 }
