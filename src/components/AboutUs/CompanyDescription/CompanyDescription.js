3d6c6b38cf4dfdf759b0cce4595ee31e2306625d
yishak <yishak.110402@gmail.com>
2024-08-06 23:47:12 +0300

    first commit

--- src/components/AboutUs/CompanyDescription/CompanyDescription.js
+++ src/components/AboutUs/CompanyDescription/CompanyDescription.js
@@ -2,9 +2,9 @@ import "./CompanyDesctiption.css"
 export default function CompanyDescription(){
     return(
         <div className="company-description">
-            {/* <h1>HALE</h1> */}
             <h3>About HALE: Human Rights and Inclusion Network</h3>
             <p>HALE: Human Rights and Inclusion Network is a civil society organization based in Ethiopia, dedicated to advancing human rights, promoting gender equality, ensuring children's rights, fostering disability inclusion, and supporting environmental sustainability. Our mission is to create a just and equitable society where marginalized and vulnerable communities are empowered and their voices are heard. We are committed to addressing human rights violations, promoting good governance, and fostering a culture of inclusion through research, public interest litigation, civic engagement, and advocacy.</p>
+            <p>Our comprehensive approach encompasses a wide range of focus areas, including gender equality, child rights, labor rights, disability inclusion, cultural rights, environmental sustainability, and sexual and reproductive health and rights (SRHR). We are also committed to promoting democracy and good governance through civic engagement and public interest litigation.</p>
         </div>
     )
 }
