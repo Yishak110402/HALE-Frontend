5608edff693a7920391d337f00c86b88fa392084
yishak <yishak.110402@gmail.com>
2024-08-09 18:33:37 +0300

    first commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,11 +9,11 @@ export default function Navbar() {
       <div className="logo">
         <img src={navLogo} alt="" />
       </div>
-      <div onClick={()=>(setNavOpen(false))} className="nav-links">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/aboutus">About Us</NavLink>
-        <NavLink to="/programs">Programs</NavLink>
-        <NavLink to="/contact">Contact Us</NavLink>
+      <div className="nav-links">
+        <NavLink onClick={()=>(setNavOpen(false))} to="/">Home</NavLink>
+        <NavLink onClick={()=>(setNavOpen(false))} to="/aboutus">About Us</NavLink>
+        <NavLink onClick={()=>(setNavOpen(false))} to="/programs">Programs</NavLink>
+        <NavLink onClick={()=>(setNavOpen(false))} to="/contact">Contact Us</NavLink>
       </div>
       <div onClick={()=>(setNavOpen((op)=>(!op)))} className="hamburger-btn">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
