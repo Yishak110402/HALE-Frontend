34c9746dc303f42ad531868c740384b28b7cb7ae
yishak <yishak.110402@gmail.com>
2024-08-11 15:09:05 +0300

    first commit

--- src/components/Resources/ResourcesList/Resource.js
+++ src/components/Resources/ResourcesList/Resource.js
@@ -1,14 +1,21 @@
-export default function Resource({resource}){
-    return(
-        <div className="resource">
-            <div>
-            <h2>{resource.name}</h2> 
-            <a href={resource.path} download={true}>Download</a>
-            </div>
-            <div>
-                <img src={resource.imgPath} alt="" />
-            </div>
-            
-        </div>
-    )
+import { useState } from "react";
+
+export default function Resource({ resource }) {
+  const [loading, setLoading] = useState(true);
+  return (
+    <div className="resource">
+      <div>
+        <h2>{resource.name}</h2>
+        <a href={resource.path} download={true}>
+          Download
+        </a>
+      </div>
+      <div>
+          <img onLoad={()=>(setLoading(false))} src={resource.imgPath} alt="" />
+         {loading &&( <div className="img-loader">
+            <div className="wheel" />
+          </div>)}
+      </div>
+    </div>
+  );
 }
