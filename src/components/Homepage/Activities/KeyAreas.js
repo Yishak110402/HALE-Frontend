726f7f3f143609d891ef0bc5f8e0eccc0a9f7895
yishak <yishak.110402@gmail.com>
2024-08-13 19:51:06 +0300

    first commit

--- src/components/Homepage/Activities/KeyAreas.js
+++ src/components/Homepage/Activities/KeyAreas.js
@@ -1,10 +1,37 @@
+import { useEffect } from "react";
 import "./KeyAreas.css";
+import { useRef } from "react";
+import { useState } from "react";
 export default function KeyAreas() {
+
+  const keyH1 = useRef()
+  const keyValues = useRef()
+  const [visible, setVisible] = useState(false)
+
+  useEffect(function(){
+    const keyValuesObserver = new IntersectionObserver((entries)=>{
+      const entry = entries[0]
+      if(entry.isIntersecting){
+        setVisible(true)
+      }
+    })
+    keyValuesObserver.observe(keyH1.current)
+    keyValuesObserver.observe(keyValues.current)
+  },[])
+  // useEffect(function(){
+  //   const keyObserver = new IntersectionObserver((entries)=>{
+  //     const entry = entries[0]
+  //     setVisible(entry.isIntersecting)
+  //   },{rootMargin:"0px"})
+  //     keyObserver.observe(keyH1.current)
+  //     keyObserver.observe(keyValues.current)    
+  // },[visible])
+
   return (
     <div className="key-areas">
-      <h1>Our Key Focus Areas</h1>
+      <h1 className={visible?"visible" :""} ref={keyH1}>Our Key Focus Areas</h1>
       <hr/>
-      <div className="lists-container">
+      <div ref={keyValues} className={`${visible ? "visible" : ""} lists-container`}>
         <div>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
             <path d="M160 0a48 48 0 1 1 0 96 48 48 0 1 1 0-96zm8 352V128h6.9c33.7 0 64.9 17.7 82.3 46.6l58.3 97c9.1 15.1 4.2 34.8-10.9 43.9s-34.8 4.2-43.9-10.9L232 256.9V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V352h0zM58.2 182.3c19.9-33.1 55.3-53.5 93.8-54.3V384h0v96c0 17.7-14.3 32-32 32s-32-14.3-32-32V384H70.2c-10.9 0-18.6-10.7-15.2-21.1L93.3 248.1 59.4 304.5c-9.1 15.1-28.8 20-43.9 10.9s-20-28.8-10.9-43.9l53.6-89.2z" />
