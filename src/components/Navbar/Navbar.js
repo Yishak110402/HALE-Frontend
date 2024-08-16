ec33431ae3acdbf7d90e242aa4d3cb4bb3cccae7
yishak <yishak.110402@gmail.com>
2024-08-17 01:23:53 +0300

    first commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -17,12 +17,13 @@ export default function Navbar() {
     [navOpen]
   );
   return (
+    <>
     <nav className={`navbar ${!navOpen ? "closed" : ""}`}>
       <div className="logo">
         <img src={navLogo} alt="" />
       </div>
       <div className="nav-links">
-        <button onClick={() => setNavOpen(false)}>X</button>
+        {/* <button onClick={() => setNavOpen(false)}>X</button> */}
         <NavLink onClick={() => setNavOpen(false)} to="/">
           Home
         </NavLink>
@@ -51,5 +52,17 @@ export default function Navbar() {
         </svg>
       </div>
     </nav>
+    <nav className="mobile-nav">
+      <div className="mobile-logo">
+        <img src={navLogo} alt="" />
+      </div>
+      <div className="menu-btn">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+          <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+        </svg>
+
+      </div>
+    </nav>
+    </>
   );
 }
