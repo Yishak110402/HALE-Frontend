19bb8c449b170c65fae3974cb97e2ed4e92e7127
yishak <yishak.110402@gmail.com>
2024-08-17 00:31:23 +0300

    first commit

--- src/components/OurTeam/TeamList/Team.js
+++ src/components/OurTeam/TeamList/Team.js
@@ -1,9 +1,21 @@
+import { useEffect, useRef, useState } from "react"
 import { useNavigate } from "react-router-dom"
 
 export default function Team({team, index}){
+    const teamRef = useRef()
     const navigate = useNavigate()
+    const [visible, setVisible] = useState(false)
+    useEffect(function(){
+        const observer = new IntersectionObserver((entries) => {
+            const entry = entries[0]
+            if(entry.isIntersecting){
+                setVisible(true)
+            }
+        },{threshold:[0.3, 1]})
+        observer.observe(teamRef.current)
+    },[])
     return(
-        <div className="team">
+        <div ref={teamRef} className={`team ${visible ? "visible" : ""}`}>
             <img src={team.image} alt=""/>
             <h2>{team.name}</h2>
             <h4>{team.position}</h4>
