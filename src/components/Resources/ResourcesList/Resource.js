21e724846c7e4d97c3c246dbff9ae3e41f0b73e9
yishak <yishak.110402@gmail.com>
2024-08-17 00:33:21 +0300

    first commit

--- src/components/Resources/ResourcesList/Resource.js
+++ src/components/Resources/ResourcesList/Resource.js
@@ -11,7 +11,7 @@ export default function Resource({ resource }) {
         }
       },
       {
-        threshold: [0.6, 1],
+        threshold: [0.2, 1],
       }
     );
     observer.observe(resourceRef.current);
