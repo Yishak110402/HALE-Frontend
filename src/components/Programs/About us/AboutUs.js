9f577f95724633e33ac37aa22af34fad0944d359
yishak <yishak.110402@gmail.com>
2024-08-17 14:50:28 +0300

    first commit

--- src/components/Programs/About us/AboutUs.js
+++ src/components/Programs/About us/AboutUs.js
@@ -4,14 +4,14 @@ export default function AboutUs(){
             <div className="about-us">
                 
                     <div className="about-us-img">
-                        <img src="./assets/learnmore-img.jpg" alt="learn more" />
+                        <img src="./assets/AboutUs/learnmore-img.jpg" alt="learn more" />
                     </div>
 
                     <div className="about-us-detail">
                         <h1>Find out more</h1>
                         <div className="link">
-                        <p>learn more about our mission and what we do</p>
-                        <p>To learn more about our team</p>
+                        <p>Learn More About Our Mission and What We Do</p>
+                        <p>To Learn More About Our Team</p>
                         <p>Contact Us</p>
                         </div>
                     </div>
