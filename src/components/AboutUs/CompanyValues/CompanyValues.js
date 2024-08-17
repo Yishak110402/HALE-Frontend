3fd3aa0848040bdc3b38fed27b428272d9633329
yishak <yishak.110402@gmail.com>
2024-08-17 21:59:40 +0300

    first commit

--- src/components/AboutUs/CompanyValues/CompanyValues.js
+++ src/components/AboutUs/CompanyValues/CompanyValues.js
@@ -1,38 +1,44 @@
 import { useEffect, useRef, useState } from "react";
 import "./CompanyValues.css";
 export default function CompanyValues() {
-  const valuesH1 = useRef()
-  const values = useRef()
-  const [visible, setVisible] = useState(false)
-  const [valuesVisible, setValuesVisible] = useState(false)
-  useEffect(function(){
-    const valuesObserver = new IntersectionObserver((entries)=>{
-      const entry = entries[0]
-      if(entry.isIntersecting){
-        setVisible(true)
+  const valuesH1 = useRef();
+  const values = useRef();
+  const [visible, setVisible] = useState(false);
+  const [valuesVisible, setValuesVisible] = useState(false);
+  useEffect(function () {
+    const valuesObserver = new IntersectionObserver((entries) => {
+      const entry = entries[0];
+      if (entry.isIntersecting) {
+        setVisible(true);
       }
-    }, {
-    })
-    valuesObserver.observe(valuesH1.current)
-  },[])
-  useEffect(function(){
-    const valuesObserver = new IntersectionObserver((entries)=>{
-      const entry = entries[0]
-      if(entry.isIntersecting){
-        setValuesVisible(true)
+    }, {});
+    valuesObserver.observe(valuesH1.current);
+  }, []);
+  useEffect(function () {
+    const valuesObserver = new IntersectionObserver(
+      (entries) => {
+        const entry = entries[0];
+        if (entry.isIntersecting) {
+          setValuesVisible(true);
+        }
+      },
+      {
+        threshold: [0.2, 1],
       }
-    },{
-      threshold:[0.2, 1]
-    })
-    valuesObserver.observe(values.current)
-  },[])
+    );
+    valuesObserver.observe(values.current);
+  }, []);
   return (
     <div className="our-values">
-      <h1 className={visible ? "visible" : ""} ref={valuesH1}>Values</h1>
-      <hr/>
-      <div ref={values} className={`values-container ${valuesVisible ? "visible" : ""}`}>
+      <h1 className={visible ? "visible" : ""} ref={valuesH1}>
+        Values
+      </h1>
+      <hr />
+      <div
+        ref={values}
+        className={`values-container ${valuesVisible ? "visible" : ""}`}>
         <div>
-          <h1>Equality and Inclusion</h1>
+          <h1>Dignity and Inclusion</h1>
           <p>
             We believe in the inherent dignity and equality of all individuals
             and are committed to promoting inclusive practices that ensure
@@ -43,13 +49,6 @@ export default function CompanyValues() {
         <div>
           <h1>Equality and Equity</h1>
           <p>We believe in equity and equal rights of all individuals</p>
-        </div>
-        <div>
-          <h1>Dignity and Respect</h1>
-          <p>
-            Upholding the inherent dignity of every individual, advocating for
-            their rights, and ensuring they are treated with respect.
-          </p>
         </div>
         <div>
           <h1>Justice and Accountability</h1>
@@ -70,7 +69,7 @@ export default function CompanyValues() {
           </p>
         </div>
         <div>
-          <h1>Transparency and Integrity</h1>
+          <h1>Integrity and Transparency</h1>
           <p>
             We operate with transparency and integrity, ensuring that our
             actions and decisions are open, honest, and accountable to the
@@ -94,18 +93,18 @@ export default function CompanyValues() {
           </p>
         </div>
         <div>
-          <h1>Respect for Cultural Heritage</h1>
-          <p>
-            We value and respect cultural diversity and work to protect the
-            cultural rights of individuals, ensuring their ability to
-            participate in cultural life and preserve their cultural heritage.
-          </p>
-        </div>
-        <div>
           <h1>Innovation</h1>
           <p>
             Encouraging creative solutions and forward-thinking approaches to
             advance human rights and inclusion.
+          </p>
+        </div>
+        <div>
+          <h1>Empowerment</h1>
+          <p>
+            We empower marginalized and vulnerable communities by providing them
+            with the tools, resources, and support needed to advocate for their
+            rights and improve their quality of life.
           </p>
         </div>
       </div>
