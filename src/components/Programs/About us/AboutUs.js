d44ce340a07f0ad4186a3f5aa73933925ae3e21e
yishak <yishak.110402@gmail.com>
2024-08-17 15:25:00 +0300

    first commit

--- src/components/Programs/About us/AboutUs.js
+++ src/components/Programs/About us/AboutUs.js
@@ -1,21 +1,19 @@
-import './AboutUs.css'
-export default function AboutUs(){
-        return(
-            <div className="about-us">
-                
-                    <div className="about-us-img">
-                        <img src="./assets/AboutUs/learnmore-img.jpg" alt="learn more" />
-                    </div>
+import "./AboutUs.css";
+export default function AboutUs() {
+  return (
+    <div className="about-us">
+      <div className="about-us-img">
+        <img src="./assets/AboutUs/learnmore-img.jpg" alt="learn more" />
+      </div>
 
-                    <div className="about-us-detail">
-                        <h1>Find out more</h1>
-                        <div className="link">
-                        <p>Learn More About Our Mission and What We Do</p>
-                        <p>To Learn More About Our Team</p>
-                        <p>Contact Us</p>
-                        </div>
-                    </div>
-                </div>
-        )
+      <div className="about-us-detail">
+        <h1>Find out more</h1>
+        <div className="link">
+          <p>Learn More About Our Mission and What We Do</p>
+          <p>To Learn More About Our Team</p>
+          <p>Contact Us</p>
+        </div>
+      </div>
+    </div>
+  );
 }
-
